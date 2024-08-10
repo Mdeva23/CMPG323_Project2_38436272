37ea00d76df45828d5a21641815db41ceb4117cc
David <davidsedumedi23@gmail.com>
2024-08-10 16:55:47 +0200

    Edited the security code

--- Program.cs
+++ Program.cs
@@ -1,8 +1,33 @@
 using CMPG323_Project2_38436272.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
+using System.Diagnostics.Metrics;
 
-var builder = WebApplication.CreateBuilder(args);
+
+//NEW CODE TO ADD AUTHENTICATION
+ValidIssuer = builder.Configuration["Jwt:ValidIssuer"],
+ValidAudience = builder.Configuration["Jwt:ValidAudience"],                    
+IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"]))
+var securityScheme = new OpenApiSecurityScheme
+{
+    Name = "Authorization",
+    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+    In = ParameterLocation.Header,
+    Type = SecuritySchemeType.ApiKey,
+    Scheme = "Bearer",
+    BearerFormat = "JWT"
+};
+
+
+
+
+
+
+var builder = WebApplication.CreateBuilder(args); //ORIGINAL CODE STARTS HERE
 
 // Add services to the container.
 
@@ -11,9 +36,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 // Add DbContext to the services container
 builder.Services.AddDbContext<Cmpg323dbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
